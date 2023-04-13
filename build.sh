f2b8e671928bb2629ec51c90cc1342f42ae2aeae
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2023-04-13 14:22:13 +0100

    Include build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+DENO_VERSION=v1.32.4
+
+docker build -t deno-build --build-arg DENO_VERSION="${DENO_VERSION}" --progress=plain --file ./Dockerfile.compile .
+DENO_BUILD_CONTAINER="$(docker create deno-build)"
+docker cp "${DENO_BUILD_CONTAINER}:/deno/target/release/deno" .
+docker rm "${CONTAINER_ID}"
